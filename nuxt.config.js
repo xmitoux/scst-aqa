5c64a589b7bce90a458e601cd68139cb051de165
mitou <xmitoux@gmail.com>
2020-08-22 16:05:24 +0000

    追加: Vuetifyインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,7 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/vuetify',
   ],
   /*
    ** Nuxt.js modules
