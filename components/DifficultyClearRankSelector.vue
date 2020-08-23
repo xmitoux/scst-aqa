fa9d9182dec5f674e3b232e3eb35979d3cf34f17
mitou <xmitoux@gmail.com>
2020-08-23 00:25:02 +0000

    追加: 難易度別クリアランクコンポーネントに全選択・全解除処理を追加
    不定チェック状態色変更用のdeepセレクタを使うためStylelintの使用をやめた

--- components/DifficultyClearRankSelector.vue
+++ components/DifficultyClearRankSelector.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <v-checkbox
-      :label="difficultyLabels[difficultyValue].name"
-      :color="difficultyLabels[difficultyValue].color"
-      :value="difficultyValue"
+      v-model="isDifficultyChecked"
+      :label="difficultyLabel.name"
+      :color="difficultyLabel.color"
+      :indeterminate="isDifficultyCheckIndeterminate"
+      :class="difficultyIndeterminateColor"
+      :style="difficultyIndeterminateColorVariable"
+      @change="selectOrUnselectAllClearRank()"
     />
+
     <div v-for="clearRankValue in clearRankValuesDesc" :key="clearRankValue">
       <v-checkbox
         v-model="tmpSelectedClearRankValues"
         :label="clearRankLabels[clearRankValue].name"
         :color="clearRankLabels[clearRankValue].color"
         :value="clearRankValue"
-        @change="changeSelection"
+        @change="changeClearRankSelection()"
       />
     </div>
   </div>
@@ -48,21 +53,76 @@ export default {
   data() {
     return {
       difficultyLabels,
-      clearRankValuesDesc,
+      clearRankValuesDesc: clearRankValuesDesc.concat(), // 定義が破壊されないようコピー
       clearRankLabels,
+      isDifficultyChecked: true,
       tmpSelectedClearRankValues: [], // props.selection変更用の一時的な配列
     };
   },
 
-  methods: {
-    changeSelection() {
-      // 選択順になっているので昇順ソート
-      this.tmpSelectedClearRankValues.sort();
+  computed: {
+    difficultyLabel() {
+      return difficultyLabels[this.difficultyValue];
+    },
 
+    isClearRankAllChecked() {
+      return (
+        this.tmpSelectedClearRankValues.length === clearRankValuesDesc.length
+      );
+    },
+
+    isDifficultyCheckIndeterminate() {
+      return (
+        !this.isClearRankAllChecked &&
+        this.tmpSelectedClearRankValues.length > 0
+      );
+    },
+
+    // 不定チェック状態の色付け用スタイル
+    // ※Vuetifyのデフォルトで灰色固定を各色に変えるために必要
+    difficultyIndeterminateColor() {
+      return {
+        'difficulty-indeterminate': this.isDifficultyCheckIndeterminate,
+      };
+    },
+
+    // 不定チェック状態の色付け用CSS変数
+    difficultyIndeterminateColorVariable() {
+      return {
+        '--difficulty-indeterminate-color': this.difficultyLabel.color,
+      };
+    },
+  },
+
+  created() {
+    // 初期表示時は全選択
+    this.tmpSelectedClearRankValues = this.clearRankValuesDesc;
+  },
+
+  methods: {
+    changeClearRankSelection() {
       // 親からv-modelで渡されたprops.selectionを変更する
       // ※propsの直接変更は許されないため必要
       this.$emit('change', this.tmpSelectedClearRankValues);
+
+      this.isDifficultyChecked = this.isClearRankAllChecked;
+    },
+
+    selectOrUnselectAllClearRank() {
+      if (this.isClearRankAllChecked) {
+        this.tmpSelectedClearRankValues = [];
+      } else {
+        this.tmpSelectedClearRankValues = this.clearRankValuesDesc;
+      }
+      this.changeClearRankSelection();
     },
   },
 };
 </script>
+
+<style scoped>
+.difficulty-indeterminate ::v-deep .mdi-minus-box {
+  /* CSS変数で不定チェック状態の灰色を難易度ごとの色に変更 */
+  color: var(--difficulty-indeterminate-color);
+}
+</style>
